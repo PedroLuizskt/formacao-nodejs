d22a029abf1c8b9127ed87511b324532602f3b5f
Pedro Luiz <pedroschuldiner@outlook.com>
2026-02-24 12:50:37 -0300

    Fix subtotal calculation in createItem function
    Updated subtotal calculation to use 'this' for accurate values.

--- 06-shopee-cart/src/services/item.js
+++ 06-shopee-cart/src/services/item.js
@@ -1,12 +1,13 @@
-//CASOS DE USO DOS ITENS
-
-// -> criar item com subtotal certo
+// CASOS DE USO DOS ITENS
 async function createItem(name, price, quantity) {
   return {
     name,
     price,
     quantity,
-    subtotal: () => price * quantity,
+    // Correção: usando 'this' para garantir que calcule com a quantidade atualizada
+    subtotal: function () {
+      return this.price * this.quantity;
+    },
   };
 }
 
