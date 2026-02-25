8742c82a34d1c847b7c7a4e74035d7260ac5d6e4
Pedro Luiz <pedroschuldiner@outlook.com>
2026-02-25 15:13:53 -0300

    Improve prompt messages and regex for QR code options

--- projeto-qrcode/src/prompts-schema/prompt-schema-qrcode.js
+++ projeto-qrcode/src/prompts-schema/prompt-schema-qrcode.js
@@ -3,15 +3,16 @@ import chalk from "chalk";
 const promptSchemaQRCode = [
   {
     name: "link",
-    description: chalk.yellow("Digite o link para gerar o QR CODE"),
+    description: chalk.yellow("Digite o link (URL) para gerar o QR CODE"),
+    required: true,
   },
   {
     name: "type",
     description: chalk.yellow(
-      "Escolha entre o tipo de QRcode (1- NORMAL ou (2- TERMINAL"
+      "Escolha o tipo de QR Code: [1] - NORMAL ou [2] - TERMINAL (Versão Reduzida)"
     ),
-    pattern: /^[1-2]+$/,
-    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    pattern: /^[1-2]$/, // Correção do Regex
+    message: chalk.red.italic("Escolha apenas entre as opções 1 e 2."),
     required: true,
   },
 ];
