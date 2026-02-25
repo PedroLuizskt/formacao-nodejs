1dc31cb02fdfe365ec6280183e28537ab33c76e1
Pedro Luiz <pedroschuldiner@outlook.com>
2026-02-25 15:15:33 -0300

    Enhance console output and error handling
    Added chalk for colored console output and improved error handling.

--- projeto-qrcode/src/index.js
+++ projeto-qrcode/src/index.js
@@ -1,13 +1,19 @@
 import prompt from "prompt";
+import chalk from "chalk";
 
 import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
-
 import createQRCode from "./services/qr-code/create.js";
 import createPassword from "./services/password/create.js";
 
 async function main() {
+  console.clear();
+  console.log(chalk.bgGreen.black.bold(" 📦 E-COMMERCE UTILITY KIT \n"));
+
   prompt.get(promptSchemaMain, async (err, choose) => {
-    if (err) console.log(err);
+    if (err) {
+      console.log(chalk.red("\n❌ Operação cancelada."));
+      return;
+    }
 
     if (choose.select == 1) await createQRCode();
     if (choose.select == 2) await createPassword();
