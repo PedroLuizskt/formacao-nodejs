c5d0f0fb1e53086cec7de1f6d923cd9c6d42b232
Pedro Luiz <pedroschuldiner@outlook.com>
2026-02-25 15:13:29 -0300

    Fix prompt message and validation pattern

--- projeto-qrcode/src/prompts-schema/prompt-schema-main.js
+++ projeto-qrcode/src/prompts-schema/prompt-schema-main.js
@@ -4,10 +4,10 @@ const promptSchemaMain = [
   {
     name: "select",
     description: chalk.yellow.bold(
-      "Escolha a ferramenta (1 - QRCODE ou (2- PASSWORD"
+      "Escolha a ferramenta: [1] - GERAR QRCODE ou [2] - GERAR SENHA"
     ),
-    pattern: /^[1-2]+$/,
-    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    pattern: /^[1-2]$/, // Correção: Exige exatamente 1 ou 2, sem repetições
+    message: chalk.red.italic("Escolha apenas entre as opções 1 e 2."),
     required: true,
   },
 ];
