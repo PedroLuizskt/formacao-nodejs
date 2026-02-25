23d0ae0486ba564fad91345b2f5d505b021c2c54
Pedro Luiz <pedroschuldiner@outlook.com>
2026-02-25 15:15:08 -0300

    Enhance password creation logging messages

--- projeto-qrcode/src/services/password/create.js
+++ projeto-qrcode/src/services/password/create.js
@@ -2,9 +2,9 @@ import chalk from "chalk";
 import handle from "./handle.js";
 
 async function createPassword() {
-  console.log(chalk.green("password"));
+  console.log(chalk.green("\n🔑 Gerando nova senha segura..."));
   const password = await handle();
-  console.log(password);
+  console.log(chalk.bgCyan.black.bold(` SENHA GERADA: ${password} `));
 }
 
 export default createPassword;
